e9252962726cc2d214d792762cecbf39a4ad649a
suhyun <suhyun@172.17.218.8>
2020-07-05 15:32:26 +0900

    created PlayFrame for the play (바둑판)

---
+++ src/main/java/edu/handong/csee/java/PlayFrame.java
@@ -0,0 +1,54 @@
+package edu.handong.csee.java;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public class PlayFrame extends JComponent implements MouseListener, MouseMotionListener {
+
+	//인터페이스 구축 
+	private JFrame playFrame = new JFrame("Connect6 - 육목");
+	
+	public PlayFrame() {
+	
+	}
+
+	
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {	}
+
+}
