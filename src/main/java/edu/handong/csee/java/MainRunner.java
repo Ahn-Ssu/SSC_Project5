15f367caddc34ade19f909b257264fa37b23a733
suhyun <suhyun@172.17.218.8>
2020-07-05 15:25:42 +0900

    the first commit

---
+++ src/main/java/edu/handong/csee/java/MainRunner.java
@@ -0,0 +1,13 @@
+package edu.handong.csee.java;
+
+public class MainRunner {
+
+	public static void main(String[] args) {
+		MainRunner myRunner = new MainRunner();
+		myRunner.run(args);
+	}
+
+	private void run(String[] args) {
+	}
+
+}
